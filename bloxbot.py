5eb80edc685d7453f84fd1b0457534b4ec37318f
Christy O'Reilly <christy@oreills.co.uk>
2021-12-12 20:06:28 +0000

    Split feature matching to function

--- bloxbot.py
+++ bloxbot.py
@@ -19,8 +19,7 @@ img2 = cv.imread('testimgs/1.png', 0)  # trainImage
 orb = cv.ORB_create(10000, 1.2, nlevels=8, edgeThreshold=5)
 
 # find the keypoints and descriptors with ORB
-keypoints1, descriptors1 = orb.detectAndCompute(img1, None)
-keypoints2, descriptors2 = orb.detectAndCompute(img2, None)
+query_keypoints, query_descriptors = orb.detectAndCompute(img1, None)
 
 
 def create_meanshift(keypoints):
@@ -38,10 +37,10 @@ def create_meanshift(keypoints):
     return ms
 
 
-def plot_matches(good_matches, keypoints1, keypoints2):
-    src_pts = np.float32([keypoints1[m.queryIdx].pt
+def plot_matches(good_matches, query_keypoints, train_keypoints):
+    src_pts = np.float32([query_keypoints[m.queryIdx].pt
                           for m in good_matches]).reshape(-1, 1, 2)
-    dst_pts = np.float32([keypoints2[m.trainIdx].pt
+    dst_pts = np.float32([train_keypoints[m.trainIdx].pt
                           for m in good_matches]).reshape(-1, 1, 2)
 
     M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC, 2)
@@ -65,55 +64,62 @@ def plot_matches(good_matches, keypoints1, keypoints2):
             matchesMask=matchesMask,  # draw only inliers
             flags=2)
 
-        img3 = cv.drawMatches(img1, keypoints1, img2_poly, keypoints2,
+        img3 = cv.drawMatches(img1, query_keypoints, img2_poly, train_keypoints,
                               good_matches, None, **draw_params)
 
         plt.imshow(img3, 'gray'), plt.show()
 
 
-meanshift = create_meanshift(keypoints2)
+def find_features_in_train_image(train_image):
+    train_keypoints, train_descriptors = orb.detectAndCompute(train_image, None)
 
-# TODO unused, look up
-# cluster_centers = ms.cluster_centers_
+    meanshift = create_meanshift(train_keypoints)
 
-labels = meanshift.labels_
-labels_unique = np.unique(labels)
-n_clusters_ = len(labels_unique)
-print("number of estimated clusters : %d" % n_clusters_)
+    # TODO unused, look up
+    # cluster_centers = ms.cluster_centers_
 
-s = [None] * n_clusters_
-for i in range(n_clusters_):
-    d, = np.where(meanshift.labels_ == i)
-    print(d.__len__())
-    s[i] = list(keypoints2[xx] for xx in d)
+    labels = meanshift.labels_
+    labels_unique = np.unique(labels)
+    n_clusters_ = len(labels_unique)
+    print("number of estimated clusters : %d" % n_clusters_)
 
-des2_ = descriptors2
+    s = [None] * n_clusters_
+    for i in range(n_clusters_):
+        d, = np.where(meanshift.labels_ == i)
+        print(d.__len__())
+        s[i] = list(train_keypoints[xx] for xx in d)
 
-for i in range(n_clusters_):
+    des2_ = train_descriptors
 
-    keypoints2 = s[i]
-    d, = np.where(meanshift.labels_ == i)
-    descriptors2 = des2_[d,]
+    for i in range(n_clusters_):
 
-    FLANN_INDEX_KDTREE = 0
-    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
-    search_params = dict(checks=50)
+        train_keypoints = s[i]
+        d, = np.where(meanshift.labels_ == i)
+        train_descriptors = des2_[d,]
 
-    flann = cv.FlannBasedMatcher(index_params, search_params)
+        FLANN_INDEX_KDTREE = 0
+        index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+        search_params = dict(checks=50)
 
-    descriptors1 = np.float32(descriptors1)
-    descriptors2 = np.float32(descriptors2)
+        flann = cv.FlannBasedMatcher(index_params, search_params)
 
-    matches = flann.knnMatch(descriptors1, descriptors2, 2)
+        query_descriptors_ = np.float32(query_descriptors)
+        train_descriptors = np.float32(train_descriptors)
 
-    # store all the good_matches matches as per Lowe's ratio test.
-    good_matches = [
-        m for (m, n) in matches if m.distance < LOWE_MATCH_RATIO * n.distance
-    ]
+        matches = flann.knnMatch(query_descriptors_, train_descriptors, 2)
 
-    if len(good_matches) > MIN_MATCH_COUNT:
-        plot_matches(good_matches, keypoints1, keypoints2)
-    else:
-        print("Not enough matches are found - %d/%d" %
-              (len(good_matches), MIN_MATCH_COUNT))
-        matchesMask = None
+        # store all the good_matches matches as per Lowe's ratio test.
+        good_matches = [
+            m for (m, n) in matches
+            if m.distance < LOWE_MATCH_RATIO * n.distance
+        ]
+
+        if len(good_matches) > MIN_MATCH_COUNT:
+            plot_matches(good_matches, query_keypoints, train_keypoints)
+        else:
+            print("Not enough matches are found - %d/%d" %
+                  (len(good_matches), MIN_MATCH_COUNT))
+            matchesMask = None
+
+
+find_features_in_train_image(img2)
